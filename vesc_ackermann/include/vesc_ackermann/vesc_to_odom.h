531565d9651e9c680879623a108ed3e1f745f3f6
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2020-02-24 09:36:30 +0100

    Port modifications from thesis of David Kopecký

--- vesc_ackermann/include/vesc_ackermann/vesc_to_odom.h
+++ vesc_ackermann/include/vesc_ackermann/vesc_to_odom.h
@@ -6,6 +6,9 @@
 #include <ros/ros.h>
 #include <vesc_msgs/VescStateStamped.h>
 #include <std_msgs/Float64.h>
+#include <std_msgs/Bool.h>
+#include <teensy/drive_values.h>
+#include <teensy/pwm_high.h>
 #include <boost/shared_ptr.hpp>
 #include <tf/transform_broadcaster.h>
 
@@ -24,6 +27,7 @@ private:
   std::string base_frame_;
   /** State message does not report servo position, so use the command instead */
   bool use_servo_cmd_;
+  bool estop_state_;
   // conversion gain and offset
   double speed_to_erpm_gain_, speed_to_erpm_offset_;
   double steering_to_servo_gain_, steering_to_servo_offset_;
@@ -32,18 +36,23 @@ private:
 
   // odometry state
   double x_, y_, yaw_;
-  std_msgs::Float64::ConstPtr last_servo_cmd_; ///< Last servo position commanded value
+  bool has_steering_cmd_; ///< Last servo position commanded value
   vesc_msgs::VescStateStamped::ConstPtr last_state_; ///< Last received state message
+  int steering_angle_;
 
   // ROS services
   ros::Publisher odom_pub_;
   ros::Subscriber vesc_state_sub_;
   ros::Subscriber servo_sub_;
+  ros::Subscriber estop_sub_;
+  ros::Subscriber pwm_high_sub;
   boost::shared_ptr<tf::TransformBroadcaster> tf_pub_;
 
   // ROS callbacks
   void vescStateCallback(const vesc_msgs::VescStateStamped::ConstPtr& state);
-  void servoCmdCallback(const std_msgs::Float64::ConstPtr& servo);
+  void servoCmdCallback(const teensy::drive_values::ConstPtr& servo);
+  void eStopCallback(const std_msgs::Bool::ConstPtr& estop);
+  void pwmHighCallback(const teensy::pwm_high::ConstPtr& pwm_high);
 };
 
 } // namespace vesc_ackermann
